20682409218f1d975a93c5c54aa8eba8e9886184
Navdeep <10390511+sivunq@users.noreply.github.com>
2024-05-29 20:48:02 +0530

    added rate limiting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const teamRoutes = require('./routes/teamRoutes');
 const connectDB= require('./db/connection');
 require('dotenv').config();
 
+const rateLimitTimeRange=5 * 60 * 1000; //in milliseconds
+const requestsLimit=10;
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: rateLimitTimeRange,
+  max: requestsLimit, // limit each IP to x requests per windowMs
+  message: 'Too many requests, please try again later',
+});
 
 const app = express();
+app.use(limiter);
 app.use(bodyParser.json());
 app.use('/api/members', teamRoutes);
 
