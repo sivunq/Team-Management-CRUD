272341757a7da357e17de93b4a8ef0bb42bb4dda
Navdeep <10390511+sivunq@users.noreply.github.com>
2024-05-29 20:44:26 +0530

    added functions to add, modify, delete team members

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -20,6 +20,7 @@ const getTeamMember = async (req, res) => {
 };
 
 const getAllTeamMembers = async (req, res) => {
+	
 	try{
 		const { page = 1, limit = 10 } = req.query;
 		const pageNumber = parseInt(page);
@@ -50,11 +51,67 @@ const getAllTeamMembers = async (req, res) => {
   }
 };
 
-const addTeamMember = async (req, res) => {};
+const addTeamMember = async (req, res) => {
 
-const editTeamMember = async (req, res) => {};
+  try {
+    const newTeamMember = new TeamMember(req.body);
 
-const deleteTeamMember = async (req, res) => {};
+    await newTeamMember.save();
+    res.status(201).json(newTeamMember);
+	logger.info('Added new team member successfuly:',JSON.stringify(newTeamMember));
+  } catch (error) {
+	logger.error(`Error while adding team member: ${error.message}`);
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ message: error.message });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const editTeamMember = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  try {
+    const updatedTeamMember = await TeamMember.findOneAndUpdate({id:id}, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTeamMember) {
+	  logger.error(`Team member ${id} not found`);
+      return res.status(404).json({ message: 'Team member not found' });
+    }
+
+    res.status(201).json(updatedTeamMember);
+	logger.info('Updated team member successfuly:',JSON.stringify(updatedTeamMember));
+  } catch (error) {
+	logger.error(`Error while updating team member ${id}: ${error.message}`);
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ message: error.message });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteTeamMember = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTeamMember = await TeamMember.findOneAndDelete({id:id});
+
+    if (!deletedTeamMember) {
+	  logger.error(`Team member ${id} not found`);
+      return res.status(404).json({ message: 'Team member not found' });
+    }
+
+    res.status(204).send();
+	logger.info(`Deleted team member ${id} successfuly`);
+  } catch (error) {
+	logger.error(`Error while deleting team member ${id}: ${error.message}`);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getTeamMember,
